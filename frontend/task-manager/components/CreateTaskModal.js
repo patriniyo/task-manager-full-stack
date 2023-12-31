1d3c9f811a53108d490c7ee091206d15cc15854d
patriniyo <patriniyo12@gmail.com>
2023-12-31 22:15:25 +0200

    Added the Alert

--- frontend/task-manager/components/CreateTaskModal.js
+++ frontend/task-manager/components/CreateTaskModal.js
@@ -1,57 +1,149 @@
-'use client';
-import React from "react";
+"use client";
+
+import React, { useState } from "react";
+import Alert from "./Alert";
 
 const CreateTaskModal = ({ closeModal }) => {
+  
+  const [title, setTitle] = useState("");
+  const [description, setDescription] =
+    useState("");
+  const [completed, setCompleted] =
+    useState(false);
+  const [showAlert, setShowAlert] = useState(false);
+  const [alertMessage, setShowAlertMessage] = useState("");
+  const [alertSuccess, setShowAlertSuccess] = useState(false);
+
+  const handleInputChange = (e) => {
+    const { name, value, type, checked } =
+      e.target;
+
+    switch (name) {
+      case "title":
+        setTitle(value);
+        break;
+      case "description":
+        setDescription(value);
+        break;
+      case "completed":
+        setCompleted(
+          type === "checkbox" ? checked : value
+        );
+        break;
+      default:
+        break;
+    }
+  };
+
+  const handleCreateTask = async (e) => {
+    e.preventDefault();
+
+    const taskData = {
+      // setting user id default value of one. 
+      // we intend to select the logged in user once 
+      // the authenticatication feature is complete
+      userId: 1,
+      title,
+      description,
+      completed
+    };
+
+    try {
+      const response = await fetch(
+        "http://localhost:5000/api/tasks",
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify(taskData),
+        }
+      );
+
+      if (response.ok) {
+        const responseData =
+          await response.json();
+        // Display succuess message
+        setShowAlert(true);
+        setShowAlertMessage(responseData.message);
+        setShowAlertSuccess(true);
+        console.log(
+          "Task created successfully:",
+          responseData
+        );
+      } else {
+        console.error(
+          "Failed to create task:",
+          response.statusText
+        );
+        const responseData = await response.json();
+        setShowAlert(true);
+        setShowAlertMessage(responseData.message);
+        setShowAlertSuccess(false);
+        console.error("Failed to create task:", response.statusText);
+      }
+    } catch (error) {
+      console.error(
+        "Error creating task:",
+        error.message
+      );
+    }
+
+    //closeModal(true);
+  };
+
   return (
+    <>
+    { showAlert && <Alert message={alertMessage} success={alertSuccess}/>}
     <div className="fixed top-0 left-0 w-full h-full flex items-center justify-center bg-black bg-opacity-50">
       <div className="bg-white rounded-md p-8 shadow-md w-96">
         <div className="flex items-center justify-between mb-4">
-          <h2 className="text-lg font-bold">Create Task</h2>
+          <h2 className="text-lg font-bold">
+            Create Task
+          </h2>
           <button
             className="text-gray-800 hover:text-black"
             onClick={() => {
-              // Call closeModal with true to close the modal
               closeModal(true);
             }}
           >
             x
           </button>
         </div>
-        <form>
+        <form onSubmit={handleCreateTask}>
           <div className="mb-4">
-            <label className="text-gray-600 block mb-1" htmlFor="title">
+            <label
+              className="text-gray-600 block mb-1"
+              htmlFor="title"
+            >
               Title
             </label>
             <input
               type="text"
               id="title"
               name="title"
+              value={title}
+              onChange={handleInputChange}
               className="border focus:border-blue-500 p-2 w-full rounded"
               required
             />
           </div>
           <div className="mb-4">
-            <label className="text-gray-600 block mb-1" htmlFor="description">
+            <label
+              className="text-gray-600 block mb-1"
+              htmlFor="description"
+            >
               Description
             </label>
             <textarea
               id="description"
               name="description"
+              value={description}
+              onChange={handleInputChange}
               rows="4"
               className="border focus:border-blue-500 p-2 w-full rounded"
               required
             ></textarea>
-          </div>
-          <div className="mb-4">
-            <label className="text-gray-600 block mb-1" htmlFor="dueDate">
-              Date
-            </label>
-            <input
-              type="date"
-              id="dueDate"
-              name="dueDate"
-              className="border focus:border-blue-500 p-2 w-full rounded"
-            />
           </div>
           <div className="mb-4 flex items-center justify-between">
             <label
@@ -60,17 +152,25 @@ const CreateTaskModal = ({ closeModal }) => {
             >
               Completed
             </label>
-            <input type="checkbox" id="completed" name="completed" />
+            <input
+              type="checkbox"
+              id="completed"
+              name="completed"
+              checked={completed}
+              onChange={handleInputChange}
+            />
           </div>
           <button
             type="submit"
             className="bg-blue-500 text-white px-6 py-2 rounded-md float-right"
           >
-            <span className="mr-2">+</span> Create Task
+            <span className="mr-2">+</span> Create
+            Task
           </button>
         </form>
       </div>
     </div>
+    </>
   );
 };
 
